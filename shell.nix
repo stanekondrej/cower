2249297ae1499247b3d2de451efd2d6c7792993d
Ondřej Staněk <viego443@proton.me>
2025-11-27 23:57:13 +0100

    add nix shell config file

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+   pkgs = import nixpkgs { config = {}; overlays = []; };
+ in
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.rustc
+    pkgs.cargo
+	pkgs.clippy
+	pkgs.openssl
+  ];
+
+  shellHook = ''
+    export RUSTUP_HOME="$PWD/.rustup"
+    export CARGO_HOME="$PWD/.cargo"
+    mkdir -p "$RUSTUP_HOME" "$CARGO_HOME"
+  '';
+}
+
